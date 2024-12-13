d57f425b80627aca0db8fd7722ba90a19225f014
Rhuan Guilherme <rhuan23032004@gmail.com>
2024-12-13 19:54:06 -0300

    Client: Começando a contrução do modal de nova transação (Utilizando Radix UI)

--- finTrack-client/src/components/Header/index.tsx
+++ finTrack-client/src/components/Header/index.tsx
@@ -1,11 +1,27 @@
 import { ButtonNewTransaction, HeaderContainer, HeaderContent } from './styled';
+import * as Dialog from '@radix-ui/react-dialog';
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src="./logo.svg" alt="" />
-        <ButtonNewTransaction>Nova Transação</ButtonNewTransaction>
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <ButtonNewTransaction>Nova Transação</ButtonNewTransaction>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay />
+
+            <Dialog.Content>
+              <Dialog.Title>Nova Transação</Dialog.Title>
+              <p>Nova Transação</p>
+              <Dialog.Close>x</Dialog.Close>
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
