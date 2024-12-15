a806630616a82a31515897d74b7baa138680e21f
Rhuan Guilherme <rhuan23032004@gmail.com>
2024-12-15 20:08:06 -0300

    Server: Mudando arquivo de inicialização do servidor fastify

--- finTrack-server/src/app.ts
+++ finTrack-server/src/app.ts
@@ -3,16 +3,8 @@ import { env } from './env';
 import { UserRoutes } from '@/http/controllers/users/router';
 import fastifyJwt from '@fastify/jwt';
 
-const app = fastify();
+export const app = fastify();
 
 app.register(fastifyJwt, { secret: env.JWT_SECRET });
 
 app.register(UserRoutes);
-
-app
-  .listen({
-    port: env.PORT,
-  })
-  .then(() => {
-    console.log('HTTP server listening in http://localhost:' + env.PORT);
-  });
