a17e67918612928fb7a718e36161f0cde98c4146
Rhuan Guilherme <rhuan23032004@gmail.com>
2024-12-17 18:15:52 -0300

    Client: Começando ajustes para comunicação com o back-end para criar novos usuários

--- finTrack-client/src/Contexts/UserContext.tsx
+++ finTrack-client/src/Contexts/UserContext.tsx
@@ -1,7 +1,14 @@
 import { createContext, ReactNode, useCallback, useState } from 'react';
 import { api } from '../lib/axios';
+import { AxiosError, isAxiosError } from 'axios';
 
 interface GetTokenSchema {
+  email: string;
+  password: string;
+}
+
+interface CreateUserSchema {
+  name: string;
   email: string;
   password: string;
 }
@@ -12,6 +19,8 @@ interface UserProviderProps {
 
 interface UserProviderType {
   fetchUsersGetToken: (data: GetTokenSchema) => Promise<void>;
+  fetchCreateUser: (data: CreateUserSchema) => Promise<void>;
+  fetchError?: AxiosError;
 }
 
 // eslint-disable-next-line react-refresh/only-export-components
@@ -19,20 +28,49 @@ export const UserContext = createContext({} as UserProviderType);
 
 export function UserProvider({ children }: UserProviderProps) {
   const [token, setToken] = useState<GetTokenSchema | null>(null);
+  const [fetchError, setFetchError] = useState<AxiosError | undefined>();
 
   const fetchUsersGetToken = useCallback(async (data: GetTokenSchema) => {
     const { email, password } = data;
 
-    const response = await api.post('/user/session', {
-      email,
-      password,
-    });
+    try {
+      const response = await api.post('/user/session', {
+        email,
+        password,
+      });
+      setToken(response.data.token);
+      console.log(token);
+    } catch (error) {
+      if (isAxiosError(error)) {
+        setFetchError(error as AxiosError);
+      } else {
+        console.error('Unexpected error:', error);
+      }
+    }
+  }, []);
 
-    setToken(response.data.token);
+  const fetchCreateUser = useCallback(async (data: CreateUserSchema) => {
+    const { email, password, name } = data;
+
+    try {
+      await api.post('/user/create', {
+        name,
+        email,
+        password,
+      });
+    } catch (error) {
+      if (isAxiosError(error)) {
+        setFetchError(error as AxiosError);
+      } else {
+        console.error('Unexpected error:', error);
+      }
+    }
   }, []);
 
   return (
-    <UserContext.Provider value={{ fetchUsersGetToken }}>
+    <UserContext.Provider
+      value={{ fetchUsersGetToken, fetchCreateUser, fetchError }}
+    >
       {children}
     </UserContext.Provider>
   );
